5cd5dd477b0b67cf0e2ffd4d2990af9b782b9b16
Ethan Hu <cosinlink@163.com>
2020-11-14 17:14:29 +0800

    feat: finish new react-todo, fix preventDefault problems

--- src/App.js
+++ src/App.js
@@ -1,59 +1,43 @@
 import React, { useState } from "react";
 import "./App.css";
-import Container from "./containers/Container";
-import { GAME_STATUS } from "./constant";
+import NewTask from "./components/NewTask";
+import TasksList from "./components/TasksList";
+const {log} = console;
 
-export const AppContext = React.createContext({});
+export default function App() {
+    // hook your code up here ;)
+    const [newTask, setNewTask] = useState({});
+    const [allTasks, setAllTasks] = useState([]);
 
-const zeroLine = (squareSize) => {
-    const line = [];
-    for (let i = 0; i < squareSize; i++) {
-        line.push(0);
-    }
-    return line;
+    const handleChange = ({ target }) => {
+        const { name, value } = target;
+        setNewTask((prevNewTask) => ({
+            ...prevNewTask,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        setAllTasks((prevTasks) => {
+            return [...prevTasks, newTask]
+        });
+        event.preventDefault();
+    };
+
+    const handleDelete = (idToRemove) =>
+        setAllTasks((prevTasks) =>
+            prevTasks.filter((item, index, arr) => item.id !== idToRemove)
+        );
+
+    return (
+        <main>
+            <h1>Tasks</h1>
+            <NewTask
+                newTask={newTask}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+            />
+            <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+        </main>
+    );
 }
-
-const zeroSquare = (squareSize) => {
-    const square = [];
-    for (let i = 0; i < squareSize; i++) {
-        square.push(zeroLine(squareSize));
-    }
-    return square;
-}
-
-function App() {
-  const squareSize = 20;
-  const [gameStatus, setGameStatus] = useState(GAME_STATUS.NOT_START);
-  const [squareData, setSquareData] = useState();
-
-  const appStart = () => {
-    // generate new mine data
-    setSquareData(zeroSquare(squareSize));
-    setGameStatus(GAME_STATUS.STARTED);
-  };
-
-  const generateSquareData = (data) => {
-      setSquareData(data);
-  }
-
-  const endGame = () => {
-    setGameStatus(GAME_STATUS.OVER);
-  };
-
-  return (
-    <AppContext.Provider
-      value={{
-        gameStatus,
-        squareData,
-        squareSize,
-      }}
-    >
-      <Container className="App"
-                 appStart={appStart}
-                 generateSquareData={generateSquareData}
-                 endGame={endGame} />
-    </AppContext.Provider>
-  );
-}
-
-export default App;
